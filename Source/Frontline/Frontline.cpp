17c9b0563a30d60c22fcb585f1a2b9ff8ab4f6c6
evancunningham369 <evancunningham369@yahoo.com>
2024-11-26 13:02:40 -0600

    Add project files.

---
+++ Source/Frontline/Frontline.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Frontline.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Frontline, "Frontline" );
