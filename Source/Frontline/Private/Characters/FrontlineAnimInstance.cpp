206c2d4652d4a1c551f0ff66edee1c4bcf01c33f
evancunningham369 <evancunningham369@yahoo.com>
2024-11-30 01:19:40 -0600

    add custom anim instance class

---
+++ Source/Frontline/Private/Characters/FrontlineAnimInstance.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/FrontlineAnimInstance.h"
+#include "Characters/FrontlineCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UFrontlineAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	FrontlineCharacter = Cast<AFrontlineCharacter>(TryGetPawnOwner());
+	if (FrontlineCharacter)
+	{
+		FrontlineCharacterMovement = FrontlineCharacter->GetCharacterMovement();
+	}
+}
+
+void UFrontlineAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (FrontlineCharacterMovement)
+	{
+		GroundSpeed = UKismetMathLibrary::VSizeXY(FrontlineCharacterMovement->Velocity);
+	}
+}
