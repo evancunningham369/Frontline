24e068149bdfbaec915bd27034fd0422086cc8d8
evancunningham369 <evancunningham369@yahoo.com>
2024-11-30 01:21:00 -0600

    Add header files

---
+++ Source/Frontline/Public/Characters/FrontlineAnimInstance.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "FrontlineAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTLINE_API UFrontlineAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class AFrontlineCharacter* FrontlineCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	class UCharacterMovementComponent* FrontlineCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
+};
--- Source/Frontline/Public/Characters/FrontlineCharacter.h
+++ Source/Frontline/Public/Characters/FrontlineCharacter.h
@@ -6,20 +6,49 @@
 #include "GameFramework/Character.h"
 #include "FrontlineCharacter.generated.h"
 
+struct FInputActionValue;
+
 UCLASS()
 class FRONTLINE_API AFrontlineCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	UPROPERTY(VisibleAnywhere)
+	class USkeletalMeshComponent* CharacterMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent* ViewCamera;
+
 public:
 	AFrontlineCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected: 
+	void Move(const FInputActionValue& Value);
+
+	void Look(const FInputActionValue& Value);
+
+	virtual void Jump() override;
 
 protected:
 	virtual void BeginPlay() override;
 
 public:	
-	virtual void Tick(float DeltaTime) override;
-
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
